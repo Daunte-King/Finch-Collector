d74b8211c74715579e56de276ce670234c0828ec
Daunte-King <dauntedking@gmail.com>
2022-04-15 16:22:55 -0600

    Finished lab part 2

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name = 'home'),
     path('about/', views.about, name = 'about'),
-    path('finchs/', views.finchs_index, name = 'index')
+    path('finchs/', views.finchs_index, name = 'index'),
+    path('finchs/<int:finch_id>/', views.finchs_detail, name = 'detail')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
+from .models import Finch
 
 # Create your views here.
 
-from django.http import HttpResponse
+
 
 def home(request):
-    return HttpResponse('<h1>Hello<h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
-def index(request):
+
+def finchs_index(request):
+    finchs = Finch.objects.all()
     return render(request, 'finchs/index.html', { 'finchs': finchs})
+
+def finchs_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finchs/detail.html', { 'finch': finch})
