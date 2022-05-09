e7ea91fd6543babf24ad8662ff77a880e91837a5
Daunte-King <dauntedking@gmail.com>
2022-05-09 14:04:03 -0600

    finished part 3

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -8,5 +9,8 @@ class Finch(models.Model):
     height = models.CharField(max_length=20)
     vertical = models.IntegerField()
 
-def _str_(self):
-    return self.name
+    def _str_(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'finch_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Finch
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 # Create your views here.
 
@@ -19,3 +20,15 @@ def finchs_index(request):
 def finchs_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
     return render(request, 'finchs/detail.html', { 'finch': finch})
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = ['name', 'height', 'vertical']
+
+class FinchUpdate(UpdateView):
+    model = Finch
+    fields = ['name', 'height', 'vertical']
+
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url = '/finchs/'
